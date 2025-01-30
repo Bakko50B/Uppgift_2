8a2381a04236cf802955d26f4b84cc8bed7be93b
Torbjörn Lundberg <torbjorn@basstrombone.info>
2025-01-30 16:16:49 +0100

    Hämtar data och skriver ut i tabellen

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,59 @@
 "use strict";
 
-const MyDateEl = document.getElementById("date");
-const today = new Date();
-const date = String(today.getDate()).padStart(2, '0');
-const month = String(today.getMonth() + 1).padStart(2, '0'); // Månaderna är 0-indexerade, så vi lägger till 1
-const year = today.getFullYear();
+let courses = [];
 
-const fullDate = `${year}-${month}-${date}`;
+window.onload = () => {
+    getCourses();
+}
+
+async function getCourses() {
+    try {
+        const response = await fetch("https://webbutveckling.miun.se/files/ramschema_ht24.json");
+        if (!response.ok) {
+            throw new Error("Fel vid anslutningen!");
+        }
+        courses = await response.json();
+        console.table(courses);
+        printCourses(courses);
+    }
+    catch (error){
+        console.error(error);
+        document.querySelector("#errormessage").innerHTML = "<p>Fel vid hämntning av informationen!</p>";
+    }
+}
 
 
-MyDateEl.innerHTML =
-`    
-    Dagens datum:   <br>
-    ${fullDate}
-`;
+function printCourses(data){
+    const myTbodyEl = document.getElementById("courseTBody");
+
+    myTbodyEl.innerHTML = "";
+
+    data.forEach(row => { 
+        myTbodyEl.innerHTML += `<tr><td> ${row.code}</td> <td> ${row.coursename}</td> <td> ${row.progression}</td></tr>`;
+
+        
+    });
+
+}
+
+
+
+
+/* Datum i footern*/
+
+// const MyDateEl = document.getElementById("date");
+// const today = new Date();
+// const date = String(today.getDate()).padStart(2, '0');
+// const month = String(today.getMonth() + 1).padStart(2, '0'); // Månaderna är 0-indexerade, så vi lägger till 1
+// const year = today.getFullYear();
+
+// const fullDate = `${year}-${month}-${date}`;
+
+
+// MyDateEl.innerHTML =
+//     `    
+//     Dagens datum:   <br>
+//     ${fullDate}
+// `;
+
+/* Slut datum*/
