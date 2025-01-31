d9b89a2b7f00f47f2f75e62f4a166bf57782c248
Torbjörn Lundberg <torbjorn@basstrombone.info>
2025-01-31 10:36:11 +0100

    gjort sortering. Började med sortering för varje typ, men(finna kvar men bortrkomenterat) Sedan en enda sortering för alla tpyer som hämtas från data-column

--- src/js/main.js
+++ src/js/main.js
@@ -1,29 +1,33 @@
 "use strict";
 
 let courses = [];
+let sortOrder = 1; // 1 = stigande, -1 = fallande
 
 window.onload = () => {
     getCourses();
 }
 
-const myCourseCodeTHEL = document.getElementById("courseCode");
-const myCourseNameTHEL = document.getElementById("courseName");
-const myCourseProgressionTHEL = document.getElementById("courseProgression");
+document.querySelectorAll("th[data-column]").forEach(th => {
+    th.addEventListener("click", () => {
+        let column = th.getAttribute("data-column");
+        sortCourses(column);
+        
+    });
+});
 
-if (myCourseCodeTHEL) {
-    myCourseCodeTHEL.addEventListener("click", () => {
-        alert("inne i myCourseCode");
+function sortCourses(column) {
+    courses.sort((a, b) => {
+        let valA = a[column].toString().toLowerCase();
+        let valB = b[column].toString().toLowerCase();
+
+        return valA > valB ? sortOrder : -sortOrder;
     });
-}
-if (myCourseNameTHEL) {
-    myCourseNameTHEL.addEventListener("click", () => {
-        alert("inne i myCourseName");
-    });
-}
-if (myCourseProgressionTHEL) {
-    myCourseProgressionTHEL.addEventListener("click", () => {
-        alert("inne i myCourseProgression");
-    });
+
+    sortOrder *= -1; // Växla sorteringsordning
+
+    printCourses(courses); // Uppdatera tabellen
+    
+    
 }
 
 async function getCourses() {
@@ -33,7 +37,7 @@ async function getCourses() {
             throw new Error("Fel vid anslutningen!");
         }
         courses = await response.json();
-        console.table(courses);
+        // console.table(courses);
         printCourses(courses);
     }
     catch (error) {
@@ -41,7 +45,6 @@ async function getCourses() {
         document.querySelector("#errormessage").innerHTML = "<p>Fel vid hämntning av informationen!</p>";
     }
 }
-
 
 function printCourses(data) {
     const myTbodyEl = document.getElementById("courseTBody");
@@ -51,15 +54,24 @@ function printCourses(data) {
 
         data.forEach(row => {
             myTbodyEl.innerHTML += `<tr><td> ${row.code}</td> <td> ${row.coursename}</td> <td> ${row.progression}</td></tr>`;
-
-
         });
     }
-
 }
 
+// function sortOnName(data) {
+//     data.sort((a, b) => a.coursename > b.coursename ? 1 : -1);
+//     console.table(data);
+// }
 
+// function sortOnProgress(data) {
+//     data.sort((a, b) => a.progression > b.progression ? 1 : -1);
+//     console.table(data);
+// }
 
+// function sortOnCode(data) {
+//     data.sort((a, b) => a.code > b.code ? 1 : -1);
+//     console.table(data);
+// }
 
 /* Datum i footern*/
 
